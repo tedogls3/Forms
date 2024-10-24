898262191fdce5efdc44823222b871454f4e7370
tedogls <tedogulisa1996@gmail.com>
2024-10-24 19:05:18 +0200

    refLogin

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import Header from "./components/Header";
 // import Signup from "./components/Signup";
-// import RefLogin from "./components/RefLogin";
-import StateLogin from "./components/StateLogin";
+import RefLogin from "./components/RefLogin";
+// import StateLogin from "./components/StateLogin";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <StateLogin />
-      {/* <RefLogin /> */}
+      {/* <StateLogin /> */}
+      <RefLogin />
       {/* <Signup /> */}
     </div>
   );
--- src/components/RefLogin.jsx
+++ src/components/RefLogin.jsx
@@ -1,14 +1,24 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function RefLogin() {
   const email = useRef();
   const password = useRef();
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
+
+    const emailIsValid = enteredEmail.includes("@");
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
 
     console.log(enteredEmail, enteredPassword);
   };
@@ -21,6 +31,9 @@ function RefLogin() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email addres.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
