c7cf74ed296a44957c564ad78dae6158c2656aea
tedogls <tedogulisa1996@gmail.com>
2024-10-24 11:37:41 +0200

    ref-login

--- src/components/RefLogin.jsx
+++ src/components/RefLogin.jsx
@@ -1,17 +1,31 @@
+import { useRef } from "react";
+
 function RefLogin() {
+  const email = useRef();
+  const password = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+
+    console.log(enteredEmail, enteredPassword);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" ref={email} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
