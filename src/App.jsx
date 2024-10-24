ba4fd3a25840b5e084650765f77de36c60c64730
tedogls <tedogulisa1996@gmail.com>
2024-10-24 11:19:28 +0200

    forms

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
