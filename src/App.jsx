e0d84f49359233a27bf4cf8c80a8b4e0cd85a9dc
tedogls <tedogulisa1996@gmail.com>
2024-10-24 13:36:19 +0200

    onBlur validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import Header from "./components/Header";
-import Signup from "./components/Signup";
+// import Signup from "./components/Signup";
 // import RefLogin from "./components/RefLogin";
-// import StateLogin from "./components/StateLogin";
+import StateLogin from "./components/StateLogin";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      {/* <StateLogin /> */}
+      <StateLogin />
       {/* <RefLogin /> */}
-      <Signup />
+      {/* <Signup /> */}
     </div>
   );
 }
--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -6,12 +6,31 @@ function StateLogin() {
     password: "",
   });
 
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
   const handleInputChange = (identifier, value) => {
     setEnteredValues((prevValues) => ({
       ...prevValues,
       [identifier]: value,
     }));
+
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false,
+    }));
   };
+
+  const handleInputBlur = (identifier) => {
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: true,
+    }));
+  };
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -34,9 +53,13 @@ function StateLogin() {
             id="email"
             type="email"
             name="email"
+            onBlur={() => handleInputBlur("email")}
             value={enteredValues.email}
             onChange={(e) => handleInputChange("email", e.target.value)}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email addres.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
